c4b09f622c68abff756c8c945c43cd44be98e54e
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-10-20 16:33:16 +0100

    Added tests for parallelized tcrossprod.

---
+++ tests/testthat/test-bptcross.R
@@ -0,0 +1,132 @@
+# Tests bpcross() for parallelized cross products.
+# library(testthat); library(BiocSingular); source("test-bptcross.R")
+
+test_that("bpcross_x_by_row works correctly", {
+    x <- matrix(runif(5000), ncol=20)
+    y <- matrix(runif(1000), ncol=20)
+    ref <- tcrossprod(x, y)
+
+    expect_equal(ref, BiocSingular:::bptcross_x_by_row(x, y, BPPARAM=SerialParam()))
+    expect_equal(ref, BiocSingular:::bptcross_x_by_row(x, y, BPPARAM=MulticoreParam(2)))
+    expect_equal(ref, BiocSingular:::bptcross_x_by_row(x, y, BPPARAM=MulticoreParam(3)))
+
+    # Only one matrix.
+    ref <- tcrossprod(x)
+    expect_equal(ref, BiocSingular:::bptcross_x_by_row(x, y=NULL, BPPARAM=SerialParam()))
+    expect_equal(ref, BiocSingular:::bptcross_x_by_row(x, y=NULL, BPPARAM=MulticoreParam(2)))
+    expect_equal(ref, BiocSingular:::bptcross_x_by_row(x, y=NULL, BPPARAM=MulticoreParam(3)))
+
+    # One of these is empty.
+    x0 <- x[,0]
+    ref <- tcrossprod(x0)
+    expect_equivalent(ref, BiocSingular:::bptcross_x_by_row(x0, y=NULL, BPPARAM=SerialParam()))
+    expect_equivalent(ref, BiocSingular:::bptcross_x_by_row(x0, y=NULL, BPPARAM=MulticoreParam(2)))
+    expect_equivalent(ref, BiocSingular:::bptcross_x_by_row(x0, y=NULL, BPPARAM=MulticoreParam(3)))
+
+    x0 <- x[0,]
+    ref <- tcrossprod(x0, y)
+    expect_equivalent(ref, BiocSingular:::bptcross_x_by_row(x0, y, BPPARAM=SerialParam()))
+    expect_equivalent(ref, BiocSingular:::bptcross_x_by_row(x0, y, BPPARAM=MulticoreParam(2)))
+    expect_equivalent(ref, BiocSingular:::bptcross_x_by_row(x0, y, BPPARAM=MulticoreParam(3)))
+
+    y0 <- y[0,]
+    ref <- tcrossprod(x, y0)
+    expect_equivalent(ref, BiocSingular:::bptcross_x_by_row(x, y0, BPPARAM=SerialParam()))
+    expect_equivalent(ref, BiocSingular:::bptcross_x_by_row(x, y0, BPPARAM=MulticoreParam(2)))
+    expect_equivalent(ref, BiocSingular:::bptcross_x_by_row(x, y0, BPPARAM=MulticoreParam(3)))
+
+    x0 <- x[0,]
+    y0 <- y[0,]
+    ref <- tcrossprod(x0, y0)
+    expect_equivalent(ref, BiocSingular:::bptcross_x_by_row(x0, y0, BPPARAM=SerialParam()))
+    expect_equivalent(ref, BiocSingular:::bptcross_x_by_row(x0, y0, BPPARAM=MulticoreParam(2)))
+    expect_equivalent(ref, BiocSingular:::bptcross_x_by_row(x0, y0, BPPARAM=MulticoreParam(3)))
+})
+
+test_that("bpcross_y_by_col works correctly", {
+    x <- matrix(runif(5000), ncol=20)
+    y <- matrix(runif(1000), ncol=20)
+    ref <- tcrossprod(x, y)
+
+    expect_equal(ref, BiocSingular:::bptcross_y_by_row(x, y, BPPARAM=SerialParam()))
+    expect_equal(ref, BiocSingular:::bptcross_y_by_row(x, y, BPPARAM=MulticoreParam(2)))
+    expect_equal(ref, BiocSingular:::bptcross_y_by_row(x, y, BPPARAM=MulticoreParam(3)))
+
+    # Handles empties okay.
+    x0 <- x[0,]
+    ref <- tcrossprod(x0, y)
+    expect_equivalent(ref, BiocSingular:::bptcross_y_by_row(x0, y, BPPARAM=SerialParam()))
+    expect_equivalent(ref, BiocSingular:::bptcross_y_by_row(x0, y, BPPARAM=MulticoreParam(2)))
+    expect_equivalent(ref, BiocSingular:::bptcross_y_by_row(x0, y, BPPARAM=MulticoreParam(3)))
+
+    y0 <- y[0,]
+    ref <- tcrossprod(x, y0)
+    expect_equivalent(ref, BiocSingular:::bptcross_y_by_row(x, y0, BPPARAM=SerialParam()))
+    expect_equivalent(ref, BiocSingular:::bptcross_y_by_row(x, y0, BPPARAM=MulticoreParam(2)))
+    expect_equivalent(ref, BiocSingular:::bptcross_y_by_row(x, y0, BPPARAM=MulticoreParam(3)))
+
+    x0 <- x[0,]
+    y0 <- y[0,]
+    ref <- tcrossprod(x0, y0)
+    expect_equivalent(ref, BiocSingular:::bptcross_y_by_row(x0, y0, BPPARAM=SerialParam()))
+    expect_equivalent(ref, BiocSingular:::bptcross_y_by_row(x0, y0, BPPARAM=MulticoreParam(2)))
+    expect_equivalent(ref, BiocSingular:::bptcross_y_by_row(x0, y0, BPPARAM=MulticoreParam(3)))
+})
+
+test_that("bptcross_by_col works correctly", {
+    x <- matrix(runif(1000), ncol=20)
+    y <- matrix(runif(5000), ncol=20)
+    ref <- tcrossprod(x, y)
+
+    expect_equal(ref, BiocSingular:::bptcross_by_col(x, y, BPPARAM=SerialParam()))
+    expect_equal(ref, BiocSingular:::bptcross_by_col(x, y, BPPARAM=MulticoreParam(2)))
+    expect_equal(ref, BiocSingular:::bptcross_by_col(x, y, BPPARAM=MulticoreParam(3)))
+
+    # Only one matrix.
+    ref <- tcrossprod(x)
+    expect_equal(ref, BiocSingular:::bptcross_by_col(x, y=NULL, BPPARAM=SerialParam()))
+    expect_equal(ref, BiocSingular:::bptcross_by_col(x, y=NULL, BPPARAM=MulticoreParam(2)))
+    expect_equal(ref, BiocSingular:::bptcross_by_col(x, y=NULL, BPPARAM=MulticoreParam(3)))
+
+    # One of these is empty.
+    x0 <- x[,0]
+    ref <- tcrossprod(x0)
+    expect_equivalent(ref, BiocSingular:::bptcross_by_col(x0, y=NULL, BPPARAM=SerialParam()))
+    expect_equivalent(ref, BiocSingular:::bptcross_by_col(x0, y=NULL, BPPARAM=MulticoreParam(2)))
+    expect_equivalent(ref, BiocSingular:::bptcross_by_col(x0, y=NULL, BPPARAM=MulticoreParam(3)))
+
+    x0 <- x[0,]
+    ref <- tcrossprod(x0, y)
+    expect_equivalent(ref, BiocSingular:::bptcross_by_col(x0, y, BPPARAM=SerialParam()))
+    expect_equivalent(ref, BiocSingular:::bptcross_by_col(x0, y, BPPARAM=MulticoreParam(2)))
+    expect_equivalent(ref, BiocSingular:::bptcross_by_col(x0, y, BPPARAM=MulticoreParam(3)))
+
+    y0 <- y[0,]
+    ref <- tcrossprod(x, y0)
+    expect_equivalent(ref, BiocSingular:::bptcross_by_col(x, y0, BPPARAM=SerialParam()))
+    expect_equivalent(ref, BiocSingular:::bptcross_by_col(x, y0, BPPARAM=MulticoreParam(2)))
+    expect_equivalent(ref, BiocSingular:::bptcross_by_col(x, y0, BPPARAM=MulticoreParam(3)))
+
+    x0 <- x[0,]
+    y0 <- y[0,]
+    ref <- tcrossprod(x0, y0)
+    expect_equivalent(ref, BiocSingular:::bptcross_by_col(x0, y0, BPPARAM=SerialParam()))
+    expect_equivalent(ref, BiocSingular:::bptcross_by_col(x0, y0, BPPARAM=MulticoreParam(2)))
+    expect_equivalent(ref, BiocSingular:::bptcross_by_col(x0, y0, BPPARAM=MulticoreParam(3)))
+})
+
+test_that("bptcross overall function works correctly", {
+    x <- matrix(runif(300), ncol=15)
+    ref <- tcrossprod(x)
+    expect_equal(ref, BiocSingular:::bptcross(x, BPPARAM=SerialParam()))
+    expect_equal(ref, BiocSingular:::bptcross(x, BPPARAM=MulticoreParam(2))) # by row, which is divisible by 2.
+    expect_equal(ref, BiocSingular:::bptcross(x, BPPARAM=MulticoreParam(3))) # by column, which is divisible by 3.
+
+    x <- matrix(runif(150), ncol=15)
+    y <- matrix(runif(300), ncol=15)
+    ref <- tcrossprod(x, y)
+    expect_equal(ref, BiocSingular:::bptcross(x, y, BPPARAM=SerialParam()))
+    expect_equal(ref, BiocSingular:::bptcross(x, y, BPPARAM=MulticoreParam(2))) # by row of 'x', which is divisible by 2.
+    expect_equal(ref, BiocSingular:::bptcross(x, y, BPPARAM=MulticoreParam(3))) # by column, which is divisible by 3.
+    expect_equal(ref, BiocSingular:::bptcross(x, y, BPPARAM=MulticoreParam(4))) # by row of 'y', which is divisible by 4.
+})
