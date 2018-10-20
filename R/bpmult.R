c27c64763d0f4dd514d59c8c2cb4b030ee8a33b9
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-10-20 17:31:04 +0100

    Refactored bpmult to consider granularity of job split.

--- R/bpmult.R
+++ R/bpmult.R
@@ -1,50 +1,85 @@
-#' @importFrom BiocParallel bplapply bpnworkers
-bpmult <- function(x, y, BPPARAM) 
+#' @importFrom BiocParallel bpnworkers bplapply SerialParam
+bpmult_x_by_row <- function(x, y, BPPARAM, njobs=NULL) {
+    per.core <- bpsplit_by_row(x, bpnworkers(BPPARAM), njobs=njobs)
+    out <- bplapply(per.core, FUN=.internal_mult, right=y, BPPARAM=BPPARAM) 
+    do.call(rbind, out)
+}
+
+#' @importFrom BiocParallel bpnworkers bplapply SerialParam
+bpmult_y_by_col <- function(x, y, BPPARAM, njobs=NULL) {
+    per.core <- bpsplit_by_col(y, bpnworkers(BPPARAM), njobs=njobs)
+    out <- bplapply(per.core, FUN=.internal_mult, left=x, BPPARAM=BPPARAM) 
+    do.call(cbind, out)
+}
+
+#' @importFrom BiocParallel bpnworkers bpmapply SerialParam
+bpmult_x_by_col <- function(x, y, BPPARAM, njobs=NULL) {
+    ncores <- bpnworkers(BPPARAM)
+    if (is.null(njobs)) {
+        njobs <- bpnjobs_by_col(x, ncores)
+    }
+
+    left.per.core <- bpsplit_by_col(x, ncores, njobs=njobs)
+    right.per.core <- bpsplit_by_row(y, ncores, njobs=njobs)
+ 
+    # Technically this would be slightly more memory efficient
+    # if we used bpiterate to immediately Reduce jobs upon completion,
+    # rather than holding all results in memory and adding them together.
+    out <- bpmapply(FUN=.internal_mult, left=left.per.core, right=right.per.core, BPPARAM=BPPARAM, SIMPLIFY=FALSE, USE.NAMES=FALSE)
+    Reduce("+", out)    
+}
+
+#' @importFrom BiocParallel bpnworkers SerialParam
+bpmult <- function(x, y, BPPARAM=SerialParam()) 
 # Parallelizing the multiplication by splitting the work by rows of 'x' or columns of 'y',
 # depending on which one yields a smaller per-worker serialization.
 { 
     ncores <- bpnworkers(BPPARAM)
-    y.not.mat <- is.null(dim(y))
-    x.not.mat <- is.null(dim(x))
-    if (ncores==1L || (x.not.mat && y.not.mat)) {
+    if (ncores==1L) {
         return(x %*% y)
-
-    } else if (y.not.mat || object.size(x) > object.size(y)) { # 'x' is larger, so we break it up.
-        
-        last <- 0L
-        inc <- ceiling(nrow(x)/ncores)
-        left <- vector("list", ncores)
-
-        for (i in seq_len(ncores)) {
-            I <- last + seq_len(inc)
-            I <- I[I <= nrow(x)]
-            left[[i]] <- x[I,,drop=FALSE]
-            last <- last + inc
-        }
-
-        out <- bplapply(FUN=.internal_mult, left, right=y, BPPARAM=BPPARAM)
-        return(do.call(rbind, out))
-
-    } else { # 'y' is larger, so we break it up instead.
-        last <- 0L
-        inc <- ceiling(ncol(y)/ncores)
-        right <- vector("list", ncores)
-
-        for (i in seq_len(ncores)) {
-            I <- last + seq_len(inc)
-            I <- I[I <= ncol(y)]
-            right[[i]] <- y[,I,drop=FALSE]
-            last <- last + inc
-        }
-
-        out <- bplapply(FUN=.internal_mult, right, left=x, BPPARAM=BPPARAM)
-        return(do.call(cbind, out))
-
     }
+
+    x <- .matrixify_by_row(x)
+    y <- .matrixify_by_col(y)
+
+    njobs_by_row <- bpnjobs_by_row(x, ncores)
+    njobs_by_col <- bpnjobs_by_col(x, ncores)
+    njobs_by_col2 <- bpnjobs_by_col(y, ncores)
+    
+    row.dev <- .deviation(njobs_by_row)
+    col.dev <- .deviation(njobs_by_col)
+    col.dev2 <- .deviation(njobs_by_col2)
+
+    if (col.dev2 < col.dev && col.dev2 < row.dev) {
+        return(bpmult_y_by_col(x, y, BPPARAM=BPPARAM))
+    }
+
+    if (col.dev < row.dev) {
+        njobs_by_row2 <- bpnjobs_by_row(y, ncores)
+        if (identical(njobs_by_col, njobs_by_row2)) { # only possible if the columns of 'x' match up to rows of 'y'.
+            return(bpmult_x_by_col(x, y, BPPARAM=BPPARAM))
+        }
+    }
+
+    return(bpmult_x_by_row(x, y, BPPARAM=BPPARAM))
 }
 
 .internal_mult <- function(left, right) 
 # Avoid problems with using 'y' in lapply on '%*%' directly.
 {
     left %*% right
+}
+
+.matrixify_by_row <- function(x) {
+    if (is.null(dim(x))) { 
+        dim(x) <- c(1L, length(x))
+    }
+    x
+}
+
+.matrixify_by_col <- function(x) {
+    if (is.null(dim(x))) { 
+        dim(x) <- c(length(x), 1L)
+    }
+    x
 }
