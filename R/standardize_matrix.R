026a084e730d9cbc466a9eb9ae7d063ac0504c3c
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-10-22 11:12:27 +0100

    Simplified standardization function.

--- R/standardize_matrix.R
+++ R/standardize_matrix.R
@@ -7,21 +7,13 @@ standardize_matrix <- function(x, center=NULL, scale=NULL)
     if (is.null(center) && is.null(scale)) {
         return(x)
     }
-    
     dx <- DelayedArray(x)
     dx <- t(dx)
 
     if (!is.null(center)) {
-        if (is.logical(center)) {
-            center <- rowMeans2(dx)
-        }
         dx <- dx - center
     }
-
     if (!is.null(scale)) {
-        if (is.logical(scale)) {
-            scale <- sqrt(rowSums(dx^2) / (ncol(dx) - 1)) # consistent with 'scale' for non-default 'center'.
-        }
         dx  <- dx/scale
     }
     
