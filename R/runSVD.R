f19d96dd8351521c4368e8e5cb4b8a82bc1328f1
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-10-22 15:37:05 +0100

    Added S4 methods for central runSVD function.

---
+++ R/runSVD.R
@@ -0,0 +1,24 @@
+#' @export
+setMethod("runSVD", "missing", function(..., BSPARAM) {
+    runSVD(..., BSPARAM=NULL)
+})
+
+#' @export
+setMethod("runSVD", "NULL", function(..., BSPARAM) {
+    runSVD(..., BSPARAM=ExactParam())
+})
+
+#' @export
+setMethod("runSVD", "ExactParam", function(..., BSPARAM) {
+    runExactSVD(..., fold=bsp_fold(BSPARAM))
+})
+
+#' @export
+setMethod("runSVD", "IrlbaParam", function(..., BSPARAM) {
+    do.call(runIrlba, c(list(..., fold=bsp_fold(BSPARAM), extra.work=ip_extra(BSPARAM)), bsp_args(BSPARAM)))
+})
+
+#' @export
+setMethod("runSVD", "RandomParam", function(..., BSPARAM) {
+    do.call(runRandomSVD, c(list(..., fold=bsp_fold(BSPARAM)), bsp_args(BSPARAM)))
+})
