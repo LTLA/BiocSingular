44c074eeda8f26784154637af4e1ab06ef59b9a0
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-10-22 13:10:28 +0100

    Use bpmatrix in runIrlba, bug fixes for cross-product.

--- R/runIrlba.R
+++ R/runIrlba.R
@@ -1,5 +1,5 @@
 #' @export
-#' @importFrom BiocParallel SerialParam bpstart bpstop
+#' @importFrom BiocParallel SerialParam bpstart bpstop bpisup
 #' @importFrom irlba irlba
 #' @importFrom utils head
 runIrlba <- function(x, k=5, nu=k, nv=k, center=NULL, scale=NULL, extra.work=7, ..., fold=5L, BPPARAM=SerialParam())
@@ -11,24 +11,23 @@ runIrlba <- function(x, k=5, nu=k, nv=k, center=NULL, scale=NULL, extra.work=7, 
                     v=matrix(0, ncol(x), 0)))
     }
 
-    args <- list(A=x, nu=nu, nv=max(nv, k),
-            work=max(k, nu, nv) + extra.work, ...)
-
-    if (bpnworkers(BPPARAM)!=1L) {
-        bpstart(BPPARAM) # lots of multiplications, so we set up the backend.
-        on.exit(bpstop(BPPARAM))
-        args$mult <- function(x, y) { as.matrix(bpmult(x, y, BPPARAM=BPPARAM)) }
-        args$fastpath <- FALSE
-    }
+    args <- list(work=max(k, nu, nv) + extra.work, ...)
 
     if (use_crossprod(x, fold)) {
         x <- standardize_matrix(x, center=center, scale=scale)
         x <- as.matrix(x) # remove once crossprod supports DAs.
-        res <- svd_via_crossprod(x, k=k, nu=nu, nv=nv, FUN=safe_svd, BPPARAM=BPPARAM)
+        res <- do.call(svd_via_crossprod, c(list(x, k=k, nu=nu, nv=nv, FUN=irlba, BPPARAM=BPPARAM), args))
+
     } else {
-        args$center <- center
-        args$scale <- scale
-        res <- do.call(irlba, args)
+        if (bpnworkers(BPPARAM)!=1L) {
+            if (!bpisup(BPPARAM)) {
+                bpstart(BPPARAM) # lots of multiplications, so we set up the backend.
+                on.exit(bpstop(BPPARAM))
+            }
+            x <- bpmatrix(x, BPPARAM)
+        }
+
+        res <- do.call(irlba, c(list(A=x, nu=nu, nv=max(k, nv), center=center, scale=scale), args))
         res$v <- res$v[,seq_len(nv),drop=FALSE]
         res$d <- head(res$d, k)
     }
