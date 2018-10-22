9bc527f3592dd09de6616a3fe1744e920c2c953c
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-10-22 18:36:55 +0100

    Removed runPCA generic.

--- R/AllGenerics.R
+++ R/AllGenerics.R
@@ -1,5 +1,2 @@
 #' @export
 setGeneric("runSVD", signature=c("BSPARAM"), function(..., BSPARAM=NULL) standardGeneric("runSVD"))
-
-#' @export
-setGeneric("runPCA", signature=c("BSPARAM"), function(..., BSPARAM=NULL) standardGeneric("runPCA"))
