0923baa28319420bcef136f2480ba0f521f5a46e
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-10-23 01:33:30 +0100

    Removed explicit parallelization in crossprod-based SVD.

--- R/svd_via_crossprod.R
+++ R/svd_via_crossprod.R
@@ -1,26 +1,27 @@
 #' @importFrom BiocParallel SerialParam
 #' @importFrom utils head
 #' @importFrom BiocGenerics nrow ncol
-svd_via_crossprod <- function(x, k, nu=k, nv=k, FUN=svd, ..., BPPARAM=SerialParam()) 
+svd_via_crossprod <- function(x, k, nu=k, nv=k, FUN=svd, ...) 
 # Computes the SVD via a crossproduct, using 'FUN' with arguments '...'. 
-# We assume that any centering/scaling of 'x' has already been applied to 'x'.
-# For this reason, we can't easily run 'FUN(x)' directly, as this might require centering/scaling within 'FUN'.
+# We assume that any centering/scaling has already been applied to 'x'.
+# For this reason, we can't easily run 'FUN(x)' directly, 
+# as this might require centering/scaling within 'FUN' (e.g., in irlba()).
 {
     if (nrow(x) > ncol(x)) {
-        y <- bpcross(x, BPPARAM=BPPARAM)
+        y <- crossprod(x)
         res <- FUN(y, nu=0, nv=max(nu, nv, k), ...)
         res$d <- sqrt(res$d)
 
-        u0 <- bpmult(x, res$v[,seq_len(nu),drop=FALSE], BPPARAM=BPPARAM)
+        u0 <- x %*% res$v[,seq_len(nu),drop=FALSE]
         res$u <- sweep(u0, 2, head(res$d, nu), "/")
         res$v <- res$v[,seq_len(nv),drop=FALSE]
 
     } else {
-        y <- bptcross(x, BPPARAM=BPPARAM)
+        y <- tcrossprod(x)
         res <- FUN(y, nu=max(nu, nv, k), nv=0, ...)
         res$d <- sqrt(res$d)
 
-        v0 <- bpcross(x, res$u[,seq_len(nv),drop=FALSE], BPPARAM=BPPARAM)
+        v0 <- crossprod(x, res$u[,seq_len(nv),drop=FALSE])
         res$v <- sweep(v0, 2, head(res$d, nv), "/")
         res$u <- res$u[,seq_len(nu),drop=FALSE]
     }
