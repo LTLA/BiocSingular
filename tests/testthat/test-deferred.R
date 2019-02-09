e15c166354685414084e1353574dce49c701a065
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2019-02-09 14:14:43 +0000

    Extended tests for refactored DeferredMatrix.

--- tests/testthat/test-deferred.R
+++ tests/testthat/test-deferred.R
@@ -1,45 +1,60 @@
 # Tests the DeferredMatrix implementation.
 # library(testthat); library(BiocSingular); source("test-deferred.R")
 
-library(BiocSingular)
-spawn_scenarios <- function(NR=50, NC=20) {
+spawn_scenarios_basic <- function(NR, NC, CREATOR, REALIZER) {
     output <- vector("list", 8)
     counter <- 1L
 
     for (trans in c(FALSE, TRUE)) {
         for (it in 1:4) {
-            y <- matrix(rnorm(NR*NC), ncol=NC)
+            y <- CREATOR(NR, NC)
+            ref <- REALIZER(y) 
             center <- scale <- NULL
     
             if (it==1L) {
-                center <- colMeans(y)
-                scale <- runif(ncol(y))
-                ref.y <- scale(y, center=center, scale=scale)
+                center <- colMeans(ref)
+                scale <- runif(ncol(ref))
+                ref <- scale(ref, center=center, scale=scale)
             } else if (it==2L) {
-                center <- rnorm(ncol(y))
-                ref.y <- scale(y, center=center, scale=FALSE)
+                center <- rnorm(ncol(ref))
+                ref <- scale(ref, center=center, scale=FALSE)
             } else if (it==3L) {
-                scale <- runif(ncol(y))
-                ref.y <- scale(y, center=FALSE, scale=scale)
-            } else {
-                ref.y <- y
+                scale <- runif(ncol(ref))
+                ref <- scale(ref, center=FALSE, scale=scale)
             }
   
             # Getting rid of excess attributes.
-            attr(ref.y, "scaled:center") <- NULL
-            attr(ref.y, "scaled:scale") <- NULL
+            attr(ref, "scaled:center") <- NULL
+            attr(ref, "scaled:scale") <- NULL
 
             def <- DeferredMatrix(y, center=center, scale=scale)
             if (trans) {
                 def <- t(def)
-                ref.y <- t(ref.y)
+                ref <- t(ref)
             }
             
-            output[[counter]] <- list(ref=ref.y, def=def)
+            output[[counter]] <- list(ref=ref, def=def)
             counter <- counter+1L
         }
     }
     output
+}
+
+spawn_scenarios <- function(NR=50, NC=20) {
+    c(
+        spawn_scenarios_basic(NR, NC,
+            CREATOR=function(r, c) {
+                matrix(rnorm(r*c), ncol=c)
+            }, 
+            REALIZER=identity
+        ),
+        spawn_scenarios_basic(NR, NC, 
+            CREATOR=function(r, c) {
+                Matrix::rsparsematrix(r, c, 0.1)
+            },
+            REALIZER=as.matrix
+        )
+    )
 }
 
 ##########################
@@ -48,17 +63,27 @@ set.seed(100001)
 test_that("DeferredMatrix utility functions work as expected", {
     possibles <- spawn_scenarios()
     for (test in possibles) {
+        expect_s4_class(test$def, "DeferredMatrix")
+
         expect_identical(dim(test$def), dim(test$ref))
         expect_identical(length(test$def), length(test$ref))
         expect_identical(as.matrix(test$def), test$ref)
 
         expect_equal(rowSums(test$def), rowSums(test$ref))
         expect_equal(colSums(test$def), colSums(test$ref))
+        expect_equal(rowMeans(test$def), rowMeans(test$ref))
+        expect_equal(colMeans(test$def), colMeans(test$ref))
+
+        tdef <- t(test$def)
+        expect_s4_class(tdef, "DeferredMatrix") # still a DefMat!
+        expect_identical(t(tdef), test$def)
+        expect_identical(as.matrix(tdef), t(test$ref))
 
         # Checking column names getting and setting.
         spawn_names <- sprintf("THING_%i", seq_len(ncol(test$def)))
         colnames(test$def) <- spawn_names
         expect_identical(spawn_names, colnames(test$def))
+        expect_s4_class(test$def, "DeferredMatrix") # still a DefMat!
     }
 
     # Checking erronious inputs.
@@ -69,24 +94,29 @@ test_that("DeferredMatrix utility functions work as expected", {
 
 set.seed(1000011)
 test_that("DeferredMatrix subsetting works as expected", {
+    expect_identical_and_defmat <- function(x, y) {
+        expect_s4_class(x, "DeferredMatrix") # class is correctly preserved by direct seed modification.
+        expect_identical(as.matrix(x), y)
+    }
+
     possibles <- spawn_scenarios()
     for (test in possibles) {
         i <- sample(nrow(test$def))
         j <- sample(ncol(test$def))
-        expect_identical(as.matrix(test$def[i,]), test$ref[i,])
-        expect_identical(as.matrix(test$def[,j]), test$ref[,j])
-        expect_identical(as.matrix(test$def[i,j]), test$ref[i,j])
+        expect_identical_and_defmat(test$def[i,], test$ref[i,])
+        expect_identical_and_defmat(test$def[,j], test$ref[,j])
+        expect_identical_and_defmat(test$def[i,j], test$ref[i,j])
         
         # Works with zero dimensions.
-        expect_identical(as.matrix(test$def[0,]), test$ref[0,])
-        expect_identical(as.matrix(test$def[,0]), test$ref[,0])
-        expect_identical(as.matrix(test$def[0,0]), test$ref[0,0])
+        expect_identical_and_defmat(test$def[0,], test$ref[0,])
+        expect_identical_and_defmat(test$def[,0], test$ref[,0])
+        expect_identical_and_defmat(test$def[0,0], test$ref[0,0])
         
         # Dimension dropping works as expected.
         expect_identical(test$def[i[1],], test$ref[i[1],])
         expect_identical(test$def[,j[1]], test$ref[,j[1]])
-        expect_identical(as.matrix(test$def[i[1],drop=FALSE]), test$ref[i[1],,drop=FALSE])
-        expect_identical(as.matrix(test$def[,j[1],drop=FALSE]), test$ref[,j[1],drop=FALSE])
+        expect_identical_and_defmat(test$def[i[1],drop=FALSE], test$ref[i[1],,drop=FALSE])
+        expect_identical_and_defmat(test$def[,j[1],drop=FALSE], test$ref[,j[1],drop=FALSE])
 
         # Transposition with subsetting works as expected.
         alt <- t(test$def)
@@ -98,49 +128,104 @@ test_that("DeferredMatrix subsetting works as expected", {
         colnames(test$def) <- spawn_names
         colnames(test$ref) <- spawn_names
         ch <- sample(spawn_names)
-        expect_identical(as.matrix(test$def[,ch]), test$ref[,ch])
+        expect_identical_and_defmat(test$def[,ch], test$ref[,ch])
     }
 })
 
 ##########################
+# Defining a class that can't do anything but get multiplied.
+# This checks that there isn't any hidden DelayedArray realization 
+# happening, which would give the same results but slower.
+
+setClass("CrippledMatrix", slots=c(x="matrix"))
+
+setMethod("dim", c("CrippledMatrix"), function(x) dim(x@x))
+
+setMethod(Matrix::colSums, c("CrippledMatrix"), function(x) colSums(x@x))
+
+setMethod(Matrix::rowSums, c("CrippledMatrix"), function(x) rowSums(x@x))
+
+setMethod("sweep", c("CrippledMatrix"), function (x, MARGIN, STATS, FUN = "-", check.margin = TRUE, ...) {
+    sweep(x@x, MARGIN, STATS, FUN, check.margin, ...)
+})
+
+setMethod("%*%", c("CrippledMatrix", "ANY"), function(x, y) x@x %*% y)
+
+setMethod("%*%", c("ANY", "CrippledMatrix"), function(x, y) x %*% y@x)
+
+setMethod("crossprod", c("CrippledMatrix", "missing"), function(x, y) crossprod(x@x))
+
+setMethod("crossprod", c("CrippledMatrix", "ANY"), function(x, y) crossprod(x@x, y))
+
+setMethod("crossprod", c("ANY", "CrippledMatrix"), function(x, y) crossprod(x, y@x))
+
+setMethod("tcrossprod", c("CrippledMatrix", "missing"), function(x, y) tcrossprod(x@x))
+
+setMethod("tcrossprod", c("CrippledMatrix", "ANY"), function(x, y) tcrossprod(x@x, y))
+
+setMethod("tcrossprod", c("ANY", "CrippledMatrix"), function(x, y) tcrossprod(x, y@x))
+
+spawn_extra_scenarios <- function(NR=50, NC=20) {
+    c(
+        spawn_scenarios(NR, NC),
+        spawn_scenarios_basic(NR, NC, 
+            CREATOR=function(r, c) { 
+                new("CrippledMatrix", x=matrix(runif(NR*NC), ncol=NC))
+            }, 
+            REALIZER=function(x) x@x
+        )
+    )
+}
+
+expect_equal_product <- function(x, y) {
+    expect_s4_class(x, "DelayedMatrix")
+    X <- as.matrix(x)
+
+    # standardize NULL dimnames.
+    if (!is.null(dimnames(X)) && lengths(dimnames(X))==0) dimnames(X) <- NULL 
+    if (!is.null(dimnames(y)) && lengths(dimnames(y))==0) dimnames(y) <- NULL 
+    expect_equal(X, y)
+}
+
+##########################
 
 test_that("DeferredMatrix right multiplication works as expected", {
-    possibles <- spawn_scenarios(100, 50)
+    possibles <- spawn_extra_scenarios(100, 50)
     for (test in possibles) {
         ref.y <- test$ref
         bs.y <- test$def
 
         # Multiply by a vector.
         z <- rnorm(ncol(ref.y))
-        expect_equal(bs.y %*% z, ref.y %*% z)
+        expect_equal_product(bs.y %*% z, ref.y %*% z)
 
         # Multiply by a matrix.
         z <- matrix(rnorm(ncol(ref.y)*10), ncol=10)
-        expect_equal(bs.y %*% z, ref.y %*% z)
+        expect_equal_product(bs.y %*% z, ref.y %*% z)
 
         # Multiply by an empty matrix.
         z <- matrix(0, ncol=0, nrow=ncol(ref.y))
-        expect_equal(bs.y %*% z, ref.y %*% z)
+        expect_equal_product(bs.y %*% z, ref.y %*% z)
     }
 })
 
 test_that("DeferredMatrix left multiplication works as expected", {
-    possibles <- spawn_scenarios(50, 80)
+    possibles <- spawn_extra_scenarios(50, 80)
     for (test in possibles) {
         ref.y <- test$ref
         bs.y <- test$def
 
         # Multiply by a vector.
         z <- rnorm(nrow(ref.y))
-        expect_equal(z %*% bs.y, z %*% ref.y)
+        expect_equal_product(z %*% bs.y, z %*% ref.y)
 
         # Multiply by a matrix.
         z <- matrix(rnorm(nrow(ref.y)*10), nrow=10)
-        expect_equal(z %*% bs.y, z %*% ref.y)
+        expect_equal_product(z %*% bs.y, z %*% ref.y)
 
         # Multiply by an empty matrix.
         z <- matrix(0, nrow=0, ncol=nrow(ref.y))
-        expect_equal(z %*% bs.y, z %*% ref.y)
+        expect_equal_product(z %*% bs.y, z %*% ref.y)
     }
 })
 
@@ -153,51 +238,51 @@ test_that("DeferredMatrix dual multiplication fails as expected", {
 ##########################
 
 test_that("DeferredMatrix lonely crossproduct works as expected", {
-    possibles <- spawn_scenarios(90, 30)
+    possibles <- spawn_extra_scenarios(90, 30)
     for (test in possibles) {
         ref.y <- test$ref
         bs.y <- test$def
-        expect_equal(crossprod(bs.y), crossprod(ref.y))
+        expect_equal_product(crossprod(bs.y), crossprod(ref.y))
     }
 })
 
 test_that("DeferredMatrix left crossproduct works as expected", {
-    possibles <- spawn_scenarios(60, 50)
+    possibles <- spawn_extra_scenarios(60, 50)
     for (test in possibles) {
         ref.y <- test$ref
         bs.y <- test$def
 
         # Multiply by a vector.
         z <- rnorm(nrow(ref.y))
-        expect_equal(crossprod(bs.y, z), crossprod(ref.y, z))
+        expect_equal_product(crossprod(bs.y, z), crossprod(ref.y, z))
 
         # Multiply by a matrix.
         z <- matrix(rnorm(nrow(ref.y)*10), ncol=10)
-        expect_equal(crossprod(bs.y, z), crossprod(ref.y, z))
+        expect_equal_product(crossprod(bs.y, z), crossprod(ref.y, z))
 
         # Multiply by an empty matrix.
         z <- matrix(0, ncol=0, nrow=nrow(ref.y))
-        expect_equal(crossprod(bs.y, z), crossprod(ref.y, z))
+        expect_equal_product(crossprod(bs.y, z), crossprod(ref.y, z))
     }
 })
 
 test_that("DeferredMatrix right crossproduct works as expected", {
-    possibles <- spawn_scenarios(40, 100)
+    possibles <- spawn_extra_scenarios(40, 100)
     for (test in possibles) {
         ref.y <- test$ref
         bs.y <- test$def
 
         # Multiply by a vector.
         z <- rnorm(nrow(ref.y))
-        expect_equal(crossprod(z, bs.y), crossprod(z, ref.y))
+        expect_equal_product(crossprod(z, bs.y), crossprod(z, ref.y))
 
         # Multiply by a matrix.
         z <- matrix(rnorm(nrow(ref.y)*10), ncol=10)
-        expect_equal(crossprod(z, bs.y), crossprod(z, ref.y))
+        expect_equal_product(crossprod(z, bs.y), crossprod(z, ref.y))
 
         # Multiply by an empty matrix.
         z <- matrix(0, ncol=0, nrow=nrow(ref.y))
-        expect_equal(crossprod(z, bs.y), crossprod(z, ref.y))
+        expect_equal_product(crossprod(z, bs.y), crossprod(z, ref.y))
     }
 })
 
@@ -210,16 +295,16 @@ test_that("DeferredMatrix dual crossprod fails as expected", {
 ##########################
 
 test_that("DeferredMatrix lonely tcrossproduct works as expected", {
-    possibles <- spawn_scenarios(50, 80)
+    possibles <- spawn_extra_scenarios(50, 80)
     for (test in possibles) {
         ref.y <- test$ref
         bs.y <- test$def
-        expect_equal(tcrossprod(bs.y), tcrossprod(ref.y))
+        expect_equal_product(tcrossprod(bs.y), tcrossprod(ref.y))
     }
 })
 
 test_that("DeferredMatrix left tcrossproduct works as expected", {
-    possibles <- spawn_scenarios(60, 70)
+    possibles <- spawn_extra_scenarios(60, 70)
     for (test in possibles) {
         ref.y <- test$ref
         bs.y <- test$def
@@ -231,31 +316,31 @@ test_that("DeferredMatrix left tcrossproduct works as expected", {
 
         # Multiply by a matrix.
         z <- matrix(rnorm(ncol(ref.y)*10), nrow=10)
-        expect_equal(tcrossprod(bs.y, z), tcrossprod(ref.y, z))
+        expect_equal_product(tcrossprod(bs.y, z), tcrossprod(ref.y, z))
 
         # Multiply by an empty matrix.
         z <- matrix(0, nrow=0, ncol=ncol(ref.y))
-        expect_equal(tcrossprod(bs.y, z), tcrossprod(ref.y, z))
+        expect_equal_product(tcrossprod(bs.y, z), tcrossprod(ref.y, z))
     }
 })
 
-test_that("DeferredMatrix right crossproduct works as expected", {
-    possibles <- spawn_scenarios(80, 50)
+test_that("DeferredMatrix right tcrossproduct works as expected", {
+    possibles <- spawn_extra_scenarios(80, 50)
     for (test in possibles) {
         ref.y <- test$ref
         bs.y <- test$def
 
         # Multiply by a vector.
         z <- rnorm(ncol(ref.y))
-        expect_equal(tcrossprod(z, bs.y), tcrossprod(z, ref.y))
+        expect_equal_product(tcrossprod(z, bs.y), tcrossprod(z, ref.y))
 
         # Multiply by a matrix.
         z <- matrix(rnorm(ncol(ref.y)*10), nrow=10)
-        expect_equal(tcrossprod(z, bs.y), tcrossprod(z, ref.y))
+        expect_equal_product(tcrossprod(z, bs.y), tcrossprod(z, ref.y))
 
         # Multiply by an empty matrix.
         z <- matrix(0, nrow=0, ncol=ncol(ref.y))
-        expect_equal(tcrossprod(z, bs.y), tcrossprod(z, ref.y))
+        expect_equal_product(tcrossprod(z, bs.y), tcrossprod(z, ref.y))
     }
 })
 
@@ -302,20 +387,20 @@ test_that("nested DeferredMatrix works as expected", {
        
         # Multiplication works.        
         y <- matrix(rnorm(20*2), ncol=2)        
-        expect_equal(ref2 %*% y, r2 %*% y)
-        expect_equal(t(y) %*% ref2, t(y) %*% r2)
+        expect_equal_product(r2 %*% y, ref2 %*% y)
+        expect_equal_product(t(y) %*% r2, t(y) %*% ref2)
 
         # Cross product.
         y <- matrix(rnorm(20*2), ncol=2)        
-        expect_equal(crossprod(ref2), crossprod(r2))
-        expect_equal(crossprod(ref2, y), crossprod(r2, y))
-        expect_equal(crossprod(y, ref2), crossprod(y, r2))
+        expect_equal_product(crossprod(r2), crossprod(ref2))
+        expect_equal_product(crossprod(r2, y), crossprod(ref2, y))
+        expect_equal_product(crossprod(y, r2), crossprod(y, ref2))
 
         # Transposed cross product.
         y <- matrix(rnorm(20*2), nrow=2) 
-        expect_equal(tcrossprod(ref2), tcrossprod(r2))
-        expect_equal(tcrossprod(ref2, y), tcrossprod(r2, y))
-        expect_equal(tcrossprod(y, ref2), tcrossprod(y, r2))
+        expect_equal_product(tcrossprod(r2), tcrossprod(ref2))
+        expect_equal_product(tcrossprod(r2, y), tcrossprod(ref2, y))
+        expect_equal_product(tcrossprod(y, r2), tcrossprod(y, ref2))
     }
 })
 
@@ -366,7 +451,7 @@ test_that("deep testing of tcrossproduct internals: scaled tcrossprod", {
     r1 <- DeferredMatrix(r, c1, s1)
     out <- BiocSingular:::.internal_tcrossprod(r1, s)
     ref <- BiocSingular:::.internal_tcrossprod(as.matrix(r1), s)
-    expect_equal(ref, out)
+    expect_equal(out, ref)
 
     # With transposition.
     c2 <- runif(NR)
@@ -375,20 +460,20 @@ test_that("deep testing of tcrossproduct internals: scaled tcrossprod", {
     r2 <- t(r2)
     out <- BiocSingular:::.internal_tcrossprod(r2, s)
     ref <- BiocSingular:::.internal_tcrossprod(as.matrix(r2), s)
-    expect_equal(ref, out)
+    expect_equal(out, ref)
 
     # Now with nested DeferredMatrices.
-    r3 <- DeferredMatrix(r1, rnorm(ncol(r1)), runif(ncol(r1)))
+    r3 <- DeferredMatrix(r1, rnorm(ncol(r1)), runif(ncol(r1))) # no transposition anywhere.
     out <- BiocSingular:::.internal_tcrossprod(r3, s)
     ref <- BiocSingular:::.internal_tcrossprod(as.matrix(r3), s)
-    expect_equal(ref, out)
+    expect_equal(out, ref)
 
-    r4 <- DeferredMatrix(r2, rnorm(ncol(r2)), runif(ncol(r2)))
+    r4 <- DeferredMatrix(r2, rnorm(ncol(r2)), runif(ncol(r2))) # inner matrix is transposed.
     out <- BiocSingular:::.internal_tcrossprod(r4, s)
     ref <- BiocSingular:::.internal_tcrossprod(as.matrix(r4), s)
-    expect_equal(ref, out)
+    expect_equal(out, ref)
 
-    r5 <- t(DeferredMatrix(t(r1), rnorm(nrow(r1)), runif(nrow(r1), 10, 20)))
+    r5 <- t(DeferredMatrix(t(r1), rnorm(nrow(r1)), runif(nrow(r1), 10, 20))) # inner and outer matrices are transposed.
     out <- BiocSingular:::.internal_tcrossprod(r5, s)
     ref <- BiocSingular:::.internal_tcrossprod(as.matrix(r5), s)
     expect_equal(ref, out)
@@ -396,18 +481,16 @@ test_that("deep testing of tcrossproduct internals: scaled tcrossprod", {
 
 ##########################
 
-library(DelayedArray)
 test_that("DelayedMatrix wrapping works", {
     possibles <- spawn_scenarios(80, 50)
     for (test in possibles) {
-        X <- DelayedArray(test$def)
-        expect_equal(as.matrix(X^2), test$ref^2)
-        expect_equal(as.matrix(X + 1), test$ref + 1)
+        expect_equal_product(test$def+1, test$ref+1)
 
-        v <- nrow(X)
-        expect_equal(as.matrix(X + v), test$ref + v)
-        expect_equal(as.matrix(X * v), test$ref * v)
+        v <- rnorm(nrow(test$def))
+        expect_equal_product(test$def+v, test$ref+v)
+        expect_equal_product(test$def*v, test$ref*v)
 
-        expect_identical(type(X), "double")
+        w <- rnorm(ncol(test$def))
+        expect_equal_product(sweep(test$def, 2, w, "*"), sweep(test$ref, 2, w, "*"))
     }
 })
