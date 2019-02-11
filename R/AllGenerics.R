d903c506d3f96bfb98239abbcfeae5e1df9d1999
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2019-02-11 17:37:20 +0000

    Convert runPCA into a S4 generic.

--- R/AllGenerics.R
+++ R/AllGenerics.R
@@ -4,3 +4,6 @@ setGeneric("runSVD", signature=c("BSPARAM"),
     function(x, k, nu=k, nv=k, center=NULL, scale=NULL, BPPARAM=SerialParam(), ..., BSPARAM=ExactParam()) 
         standardGeneric("runSVD")
 )
+
+#' @export
+setGeneric("runPCA", signature=c("x"), function(x, ...) standardGeneric("runPCA"))
--- R/runPCA.R
+++ R/runPCA.R
@@ -1,11 +1,11 @@
 #' @export
-runPCA <- function(x, rank, center=TRUE, scale=FALSE, BSPARAM=ExactParam(), get.rotation=TRUE, get.pcs=TRUE, ...) 
+setMethod("runPCA", "ANY", function(x, rank, center=TRUE, scale=FALSE, get.rotation=TRUE, get.pcs=TRUE, ...) 
 # Converts SVD results to PCA results.
 {
     svd.out <- runSVD(x, k=rank, 
         nu=ifelse(get.pcs, rank, 0),
         nv=ifelse(get.rotation, rank, 0),
-        center=center, scale=scale, BSPARAM=BSPARAM, ...)
+        center=center, scale=scale, ...)
 
     out <- list(sdev=svd.out$d / sqrt(nrow(x) - 1))
     if (get.rotation) {
@@ -17,4 +17,4 @@ runPCA <- function(x, rank, center=TRUE, scale=FALSE, BSPARAM=ExactParam(), get.
         colnames(out$x) <- sprintf("PC%i", seq_len(ncol(out$x)))
     }
     out                          
-}
+})
