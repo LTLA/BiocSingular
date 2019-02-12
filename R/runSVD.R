a4871586e4be52e4bc9608f5eb49b41395c4c71f
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2019-02-12 09:22:27 +0000

    Streamlined runSVD method definitions to avoid retyping.

--- R/runSVD.R
+++ R/runSVD.R
@@ -1,39 +1,39 @@
-#' @export
 #' @importFrom BiocParallel SerialParam
-setMethod("runSVD", "missing", function(x, k, nu=k, nv=k, center=NULL, scale=NULL, BPPARAM=SerialParam(), ..., BSPARAM) {
-    runSVD(x=x, k=k, nu=nu, nv=nv, center=center, scale=scale, BPPARAM=BPPARAM, ..., BSPARAM=BSPARAM)
-})
-
-#' @export
-#' @importFrom BiocParallel SerialParam
-setMethod("runSVD", "ExactParam", function(x, k, nu=k, nv=k, center=NULL, scale=NULL, BPPARAM=SerialParam(), ..., BSPARAM) {
-    runExactSVD(x=x, k=k, nu=nu, nv=nv, center=center, scale=scale, BPPARAM=BPPARAM, ..., deferred=bsdeferred(BSPARAM), fold=bsfold(BSPARAM))
-})
-
-#' @export
-#' @importFrom BiocParallel SerialParam
-setMethod("runSVD", "IrlbaParam", function(x, k, nu=k, nv=k, center=NULL, scale=NULL, BPPARAM=SerialParam(), ..., BSPARAM) {
-    do.call(runIrlbaSVD, 
-        c(
-            list(
-                x=x, k=k, nu=nu, nv=nv, center=center, scale=scale, BPPARAM=BPPARAM, ...,
-                deferred=bsdeferred(BSPARAM), fold=bsfold(BSPARAM), extra.work=ip_extra(BSPARAM)
-            ), 
-            bsargs(BSPARAM)
-         )
-    )
-})
-
-#' @export
-#' @importFrom BiocParallel SerialParam
-setMethod("runSVD", "RandomParam", function(x, k, nu=k, nv=k, center=NULL, scale=NULL, BPPARAM=SerialParam(), ..., BSPARAM) {
-    do.call(runRandomSVD, 
-        c(
-            list(
-                x=x, k=k, nu=nu, nv=nv, center=center, scale=scale, BPPARAM=BPPARAM, ...,
-                deferred=bsdeferred(BSPARAM), fold=bsfold(BSPARAM)
-            ), 
-            bsargs(BSPARAM)
+.FUN_GENERATOR <- function(FUN, ARGS) 
+# Generates the function to be used in the method,
+# to avoid re-typing the whole signature.
+{
+    function(x, k, nu=k, nv=k, center=NULL, scale=NULL, BPPARAM=SerialParam(), ..., BSPARAM) {
+        do.call(FUN, 
+            c(
+                list(x=x, k=k, nu=nu, nv=nv, center=center, scale=scale, BPPARAM=BPPARAM, ...),
+                ARGS(BSPARAM)
+             )
         )
-    )
-})
+    }
+}
+
+#' @export
+# Uses the default ExactParam() in the generic.
+setMethod("runSVD", "missing", .FUN_GENERATOR(runSVD, 
+    function(BSPARAM) list(BSPARAM=BSPARAM) 
+))
+
+#' @export
+setMethod("runSVD", "ExactParam", .FUN_GENERATOR(runExactSVD, 
+    function(BSPARAM) list(deferred=bsdeferred(BSPARAM), fold=bsfold(BSPARAM))
+))
+
+#' @export
+setMethod("runSVD", "IrlbaParam", .FUN_GENERATOR(runIrlbaSVD, 
+    function(BSPARAM) {
+        c(list(deferred=bsdeferred(BSPARAM), fold=bsfold(BSPARAM), extra.work=ip_extra(BSPARAM)), bsargs(BSPARAM))
+    }
+))
+
+#' @export
+setMethod("runSVD", "RandomParam", .FUN_GENERATOR(runRandomSVD, 
+    function(BSPARAM) {            
+        c(list(deferred=bsdeferred(BSPARAM), fold=bsfold(BSPARAM)), bsargs(BSPARAM))
+    }
+))
