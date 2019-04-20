a4013f1c95f5ee85e5817dbe57b38d83b54e6a40
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2019-04-19 20:41:59 -0700

    Protect against numerical precision issues on Win32.

--- tests/testthat/test-lowrank.R
+++ tests/testthat/test-lowrank.R
@@ -29,10 +29,22 @@ spawn_scenarios <- function(NR=50, NC=20, NP=10) {
 
 ##########################
 
+expect_identical2 <- function(x, y) 
+# Special check to avoid problems with tcrossprod precision
+# on Windows 32-bit when one of the matrices is a dgCMatrix.
+{
+    if (.Platform$OS.type=="windows") {
+        FUN <- expect_equal
+    } else {
+        FUN <- expect_identical
+    }
+    FUN(x, y)
+}
+
 expect_identical_unnamed <- function(x, y) {
     if (all(lengths(dimnames(x))==0L)) dimnames(x) <- NULL
     if (all(lengths(dimnames(y))==0L)) dimnames(y) <- NULL
-    expect_identical(x, y)
+    expect_identical2(x, y)
 }
 
 set.seed(500001)
@@ -40,23 +52,23 @@ test_that("LowRankMatrix utility functions work as expected", {
     possibles <- spawn_scenarios()
     for (test in possibles) {
         expect_s4_class(test$lrm, "LowRankMatrix")
-        expect_identical(test$lrm, LowRankMatrix(DelayedArray::seed(test$lrm)))
+        expect_identical2(test$lrm, LowRankMatrix(DelayedArray::seed(test$lrm)))
 
-        expect_identical(dim(test$lrm), dim(test$ref))
-        expect_identical(extract_array(test$lrm, list(1:10, 1:10)), test$ref[1:10, 1:10])
-        expect_identical(extract_array(test$lrm, list(1:10, NULL)), test$ref[1:10,])
-        expect_identical(extract_array(test$lrm, list(NULL, 1:10)), test$ref[,1:10])
+        expect_identical2(dim(test$lrm), dim(test$ref))
+        expect_identical2(extract_array(test$lrm, list(1:10, 1:10)), test$ref[1:10, 1:10])
+        expect_identical2(extract_array(test$lrm, list(1:10, NULL)), test$ref[1:10,])
+        expect_identical2(extract_array(test$lrm, list(NULL, 1:10)), test$ref[,1:10])
         expect_identical_unnamed(as.matrix(test$lrm), test$ref)
 
         ttest <- t(test$lrm)
         expect_s4_class(ttest, "LowRankMatrix") # still a LRMat!
-        expect_identical(t(ttest), test$lrm)
+        expect_identical2(t(ttest), test$lrm)
         expect_identical_unnamed(as.matrix(ttest), t(test$ref))
 
         # Checking column names getting and setting.
         spawn_names <- sprintf("THING_%i", seq_len(ncol(test$lrm)))
         colnames(test$lrm) <- spawn_names
-        expect_identical(spawn_names, colnames(test$lrm))
+        expect_identical2(spawn_names, colnames(test$lrm))
         expect_s4_class(test$lrm, "LowRankMatrix") # still a LRMat!
     }
 })
@@ -82,15 +94,15 @@ test_that("LowRankMatrix subsetting works as expected", {
         expect_identical_and_lrmmat(test$lrm[0,0], test$ref[0,0])
         
         # Dimension dropping works as expected.
-        expect_identical(test$lrm[i[1],], test$ref[i[1],])
-        expect_identical(test$lrm[,j[1]], test$ref[,j[1]])
+        expect_identical2(test$lrm[i[1],], test$ref[i[1],])
+        expect_identical2(test$lrm[,j[1]], test$ref[,j[1]])
         expect_identical_and_lrmmat(test$lrm[i[1],drop=FALSE], test$ref[i[1],,drop=FALSE])
         expect_identical_and_lrmmat(test$lrm[,j[1],drop=FALSE], test$ref[,j[1],drop=FALSE])
 
         # Transposition with subsetting works as expected.
         alt <- t(test$lrm)
-        expect_identical(t(alt[,i]), test$lrm[i,])
-        expect_identical(t(alt[j,]), test$lrm[,j])
+        expect_identical2(t(alt[,i]), test$lrm[i,])
+        expect_identical2(t(alt[j,]), test$lrm[,j])
 
         # Subsetting behaves with column names.
         spawn_names <- sprintf("THING_%i", seq_len(ncol(test$lrm)))
@@ -105,22 +117,22 @@ set.seed(500003)
 test_that("Silly inputs into LowRankMatrix work as expected", {
     # Default constructor works with different inputs.
     default <- LowRankMatrix()
-    expect_identical(dim(default), c(0L, 0L))
+    expect_identical2(dim(default), c(0L, 0L))
     val <- as.matrix(default)
     dimnames(val) <- NULL
-    expect_identical(val, matrix(0, 0, 0))
+    expect_identical2(val, matrix(0, 0, 0))
 
     default <- LowRankMatrix(rotation=cbind(1:5))
-    expect_identical(dim(default), c(5L, 0L))
+    expect_identical2(dim(default), c(5L, 0L))
     val <- as.matrix(default)
     dimnames(val) <- NULL
-    expect_identical(val, matrix(0, 5, 0))
+    expect_identical2(val, matrix(0, 5, 0))
     
     default <- LowRankMatrix(components=cbind(1:5))
-    expect_identical(dim(default), c(0L, 5L))
+    expect_identical2(dim(default), c(0L, 5L))
     val <- as.matrix(default)
     dimnames(val) <- NULL
-    expect_identical(val, matrix(0, 0, 5))
+    expect_identical2(val, matrix(0, 0, 5))
 
     # Checking erronious inputs.
     expect_error(LowRankMatrix(1, 1), "must be matrix-like")
